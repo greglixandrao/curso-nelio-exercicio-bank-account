421b05e2bea53e6e72e7986cf9329b8bae5e16b0
Greg Lixandrão <contato@greglixandrao.com>
2023-06-29 12:10:11 -0300

    Add final program with 2 files

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,42 @@
-package application;public class Program {
+package application;
+
+import entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+        int accountNumber = scan.nextInt();
+
+        System.out.print("Enter account holder: ");
+        scan.nextLine();
+        String name = scan.nextLine();
+
+        double value = 0;
+        System.out.print("Is the a initial deposit (y/n)? ");
+        char answer = scan.next().charAt(0);
+        if (answer == 'y') {
+            System.out.print("Enter initial deposit value: ");
+            value += scan.nextDouble();
+        }
+
+        Account account = new Account(accountNumber, name, value);
+
+        System.out.println("Account data: " + account);
+
+        System.out.println("Enter a deposit value: ");
+        account.deposit(scan.nextDouble());
+        System.out.println("Update account data: " + account);
+
+        System.out.println("Enter a withdraw value: ");
+        account.withdraw(scan.nextDouble());
+        System.out.println("Update account data: " + account);
+
+        scan.close();
+    }
 }
--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,2 +1,51 @@
-package entities;public class Account {
+package entities;
+
+public class Account {
+    private int accountNumber;
+    private String name;
+    private double balance;
+    public Account(int accountNumber, String name, double initialDeposit) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        deposit(initialDeposit);
+    }
+
+    public Account(int accountNumber, String name) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+    public void withdraw(double amount) {
+        balance -= amount + 5.00;
+    }
+
+    public String toString(){
+        return "Account "
+                + accountNumber
+                + ", Holder: "
+                + name
+                + ", Balance: $ "
+                +String.format("%.2f", balance);
+    }
 }
+
